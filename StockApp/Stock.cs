c5a8f12a1d4ce882af1b327e8a70f61010787d28
nmgrant <ngrant40@gmail.com>
2016-02-09 10:59:44 -0800

    Added value change to Stock thread

--- StockApp/Stock.cs
+++ StockApp/Stock.cs
@@ -10,18 +10,25 @@ namespace StockApp {
 
         private String name;
         private readonly int initialValue;
+        private int currentValue;
+        private int changeOfValue;
         private readonly int threshold;
         private readonly int maxChange;
 
-        public Stock(String name, int initialValue, int threshold, int maxChange) {
+        public Stock(String name, int initialValue, int maxChange, int threshold) {
             this.name = name;
             this.initialValue = initialValue;
             this.threshold = threshold;
             this.maxChange = maxChange;
+            currentValue = initialValue;
+            changeOfValue = 0;
             // Initializes a thread using a lambda expression as the run behavior
             Thread thread = new Thread(() => {
-                Thread.Sleep(500);
-                Console.WriteLine(this.name);
+                for (int i = 0; i < 3; i ++) {
+                    changeOfValue += new Random().Next(-maxChange, maxChange);
+                    Console.WriteLine("{0} value before: {1} after: {2}", name, currentValue, currentValue += changeOfValue);
+                    Thread.Sleep(500);
+                }
             });
             thread.Start();
         }

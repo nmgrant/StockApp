d45fe10ba2374e855db79bb467ff55220d313c52
nmgrant <ngrant40@gmail.com>
2016-02-09 10:31:03 -0800

    Added constructors

--- StockApp/Stock.cs
+++ StockApp/Stock.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,5 +7,18 @@ using System.Threading.Tasks;
 
 namespace StockApp {
     class Stock {
+
+        private String name;
+        private readonly int initialValue;
+        private readonly int threshold;
+        private readonly int maxChange;
+
+        public Stock(String name, int initialValue, int threshold, int maxChange) {
+            this.name = name;
+            this.initialValue = initialValue;
+            this.threshold = threshold;
+            this.maxChange = maxChange;
+            //Thread thread = new Thread()
+        }
     }
 }
--- StockApp/StockBroker.cs
+++ StockApp/StockBroker.cs
@@ -6,5 +6,16 @@ using System.Threading.Tasks;
 
 namespace StockApp {
     class StockBroker {
+
+        private String name;
+        private List<Stock> stocks;
+
+        public StockBroker(String name) {
+            this.name = name;
+        }
+
+        public void AddStock(Stock stock) {
+            stocks.Add(stock);
+        }
     }
 }

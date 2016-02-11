3ee85cb84cb9e3995cd62d6eabe0e4a81fbcd3c0
Evan McNaughtan <evan4james@yahoo.com>
2016-02-11 09:53:09 -0800

    added stockThresholdreached

--- StockApp/StockBroker.cs
+++ StockApp/StockBroker.cs
@@ -4,16 +4,22 @@ using System.Collections.Generic;
 namespace CECS475.Assignment3 {
     class StockBroker {
 
-        private String name;
+        private String brokerName;
         private List<Stock> stocks;
 
         public StockBroker(String name) {
-            this.name = name;
+            this.brokerName = name;
             stocks = new List<Stock>();
         }
 
         public void AddStock(Stock stock) {
+            stock.stockEvent += stockThresholdReached;
             stocks.Add(stock);
+        }
+
+        static void stockThresholdReached(object sender, StockNotificationEventArgs e) {
+            Console.WriteLine("The threshold of {0} was reached at {1}.", e.Name, e.NumberOfChanges);
+            Environment.Exit(0);
         }
     }
 }
